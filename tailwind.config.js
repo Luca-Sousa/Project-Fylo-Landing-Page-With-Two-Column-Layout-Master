60e23e6eae44f3aa2146f5d7844d4d1cf3dd5196
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-06-07 18:46:33 -0300

    Add Responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
         'col-BB': 'hsl(224, 93%, 58%)',
         'col-MC': 'hsl(170, 45%, 43%)',
         'col-LGB': 'hsl(240, 75%, 98%)',
-        'col-LG': 'hsl(0, 0%, 75%)'
+        'col-LG': 'hsl(0, 0%, 75%)',
+        'hv-btn1': 'rgba(107,155,255,255)',
+        'hv-btn2': 'rgba(105,155,254,255)'
       },
       fontFamily: {
         'Raleway': 'Raleway',
